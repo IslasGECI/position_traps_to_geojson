ac0a013ed0a76f04e077a3ac8a8388950064e5d2
Xavier Paredes <jxavierpp1@gmail.com>
2022-01-26 19:58:34 +0000

    🧪 Add pandas_to_geojson failing test

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -7,3 +7,15 @@ def test_add_offset():
     expected = augend + addend
     obtained = ptg.add_offset(augend, addend)
     assert expected == obtained
+
+
+def test_pandas_to_geojson():
+    expected = [
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [125.6, 10.1]},
+            "properties": {"is_active": "true", "date": "2021-01-23"},
+        }
+    ]
+    obtained = ptg.pandas_to_geojson()
+    assert expected == obtained
