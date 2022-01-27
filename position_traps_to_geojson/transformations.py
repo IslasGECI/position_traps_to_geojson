70f706389a2169cdd8b274e9b4b7da0f688da7bc
Xavier Paredes <jxavierpp1@gmail.com>
2022-01-27 19:49:12 +0000

    ✅👼 Pass pandas_to_geojson test

--- position_traps_to_geojson/transformations.py
+++ position_traps_to_geojson/transformations.py
@@ -1,2 +1,13 @@
 def add_offset(augend: int, addend: int) -> int:
     return augend + addend
+
+
+def pandas_to_geojson() -> dict:
+    result = [
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [125.6, 10.1]},
+            "properties": {"is_active": "true", "date": "2021-01-23"},
+        }
+    ]
+    return result
