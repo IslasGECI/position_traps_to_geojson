01ee7cc4226cc4bc910e3bd76168dec83ae74bf3
Xavier Paredes <jxavierpp1@gmail.com>
2022-02-01 19:51:49 +0000

    🧪 Make to faill pandas_to_geojson test

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -1,4 +1,5 @@
 from position_traps_to_geojson import pandas_to_geojson
+import pandas as pd
 
 
 def test_pandas_to_geojson():
@@ -18,3 +19,26 @@ def test_pandas_to_geojson():
     ]
     obtained = pandas_to_geojson()
     assert expected == obtained
+    dataframe = pd.DataFrame.from_dict(
+        {
+            "lat": [-118.28],
+            "long": [28.87],
+            "is_active": [True],
+            "date": ["2021-04-06"],
+            "id": ["TC-01-003-AC"],
+        }
+    )
+    expected = [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-118.28, 28.87],
+            },
+            "properties": {
+                "is_active": "true",
+                "date": "2021-04-06",
+                "id": "TC-01-003-AC",
+            },
+        }
+    ]
