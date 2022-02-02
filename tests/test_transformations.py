eac54613615bbf0df85615fb64a28bf315e14229
Xavier Paredes <jxavierpp1@gmail.com>
2022-02-02 19:53:44 +0000

    🧪 Add a new failing pandas_to_geojson_list test

--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -130,3 +130,54 @@ def test_pandas_to_geojson():
     ]
     obtained = pandas_to_geojson(dataframe)
     assert expected == obtained
+
+
+def test_pandas_to_geojson_list():
+    dataframe = pd.DataFrame.from_dict(
+        [
+            {
+                "lat": [-118.28],
+                "long": [28.87],
+                "is_active": [False],
+                "date": ["2022-02-02"],
+                "id": ["TC-01-003-XP"],
+            },
+            {
+                "lat": [-178.28],
+                "long": [38.87],
+                "is_active": [True],
+                "date": ["2022-03-08"],
+                "id": ["TC-01-004-NR"],
+            },
+        ]
+    )
+    expected = [
+        [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-118.28, 28.87],
+                },
+                "properties": {
+                    "is_active": False,
+                    "date": "2022-02-02",
+                    "id": "TC-01-003-XP",
+                },
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [-178.28, 38.87],
+                },
+                "properties": {
+                    "is_active": True,
+                    "date": "2022-03-08",
+                    "id": "TC-01-004-NR",
+                },
+            },
+        ]
+    ]
+    obtained = pandas_to_geojson(dataframe)
+    assert expected == obtained
