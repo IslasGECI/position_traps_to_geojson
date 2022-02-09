831a4b67a8ec3e98e0bcddd8632a49025caeeae0
Xavier Paredes <jxavierpp1@gmail.com>
2022-02-09 19:52:24 +0000

    ♻️⚡️ Improve names

--- position_traps_to_geojson/transformations.py
+++ position_traps_to_geojson/transformations.py
@@ -1,19 +1,19 @@
 def pandas_to_geojson(dataframe) -> list[dict]:
     list_of_dictionary = dataframe.to_dict("records")
-    result = [la_funcion(dictionary) for dictionary in list_of_dictionary]
-    return {"type": "FeatureCollection", "features": result}
+    feature_collection = [dictionary_to_geojson(dictionary) for dictionary in list_of_dictionary]
+    return {"type": "FeatureCollection", "features": feature_collection}
 
 
-def la_funcion(result2):
+def dictionary_to_geojson(row: dict) -> dict :
     return {
         "type": "Feature",
         "geometry": {
             "type": "Point",
-            "coordinates": [result2["lat"][0], result2["long"][0]],
+            "coordinates": [row["lat"][0], row["long"][0]],
         },
         "properties": {
-            "is_active": result2["is_active"][0],
-            "date": result2["date"][0],
-            "id": result2["id"][0],
+            "is_active": row["is_active"][0],
+            "date": row["date"][0],
+            "id": row["id"][0],
         },
     }
