2764ed6563114ab0a22c057961cdb46d6888da42
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-11-01 14:44:07 -0700

    🎨📈 Add sea color and markersize

--- position_traps_to_geojson/plot_position_traps.py
+++ position_traps_to_geojson/plot_position_traps.py
@@ -12,10 +12,12 @@ class LittleMap:
         self.geopandas = self._add_geometry(original_data)
 
     def plot(self, output_path):
-        self.geopandas.plot()
+        fig, ax = plt.subplots()
+        plt.gca().set_facecolor("#E6FFFF")
+        self.geopandas.plot(ax=ax)
         if isinstance(self.island, geopandas.GeoDataFrame):
-            ax = self.island.plot(edgecolor="black", facecolor="#fffae6")
-            self.geopandas.plot(ax=ax)
+            ax1 = self.island.plot(ax=ax,edgecolor="black", facecolor="#fffae6")
+            self.geopandas.plot(ax=ax1, color="blue", markersize=4)
         plt.savefig(output_path)
 
     def read_island(self, shp_path):
@@ -25,3 +27,10 @@ class LittleMap:
         return geopandas.GeoDataFrame(
             df, geometry=geopandas.points_from_xy(df["Coor-X"], df["Coor-Y"])
         )
+
+    def margins_from_polygon(
+    geoambiental_polygon, gap_left=100, gap_right=100, gap_up=100, gap_down=100, n_points=100
+):
+        margin_x = margins_from_coordinate(geoambiental_polygon.x, gap_left, gap_right, n_points)
+        margin_y = margins_from_coordinate(geoambiental_polygon.y, gap_down, gap_up, n_points)
+        return margin_x, margin_y
--- tests/test_plot_position_traps.py
+++ tests/test_plot_position_traps.py
@@ -19,14 +19,14 @@ class Test_LittleMap:
 
     def test_plot_only_traps(self):
         self.mapita.plot("salidita.png")
-        expected_hash = "01df0b13f403016caf4c9de2d622d86a"
+        expected_hash = "b4861eaf2c883ebb53428cb9d2a0b8fa"
         obtained_hash = self._make_hash("salidita.png")
         assert obtained_hash == expected_hash
 
     def test_plot_island_with_traps(self):
         self.mapita.read_island("tests/data/linea_costa_isla_guadalupe.shp")
         self.mapita.plot("salidita_con_isla.png")
-        expected_hash = "2b326af621af7eba167c4ed3cecbef1a"
+        expected_hash = "740045a376032b2d14d1884a3a71d15d"
         obtained_hash = self._make_hash("salidita_con_isla.png", remove=False)
         assert obtained_hash == expected_hash
 
