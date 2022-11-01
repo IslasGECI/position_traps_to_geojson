b0187ac2d5f20ba840fac6d13f7ec0a096347dc5
Guillermo Olvera <guillermo.olvera@islas.org.mx>
2022-11-01 11:54:52 -0700

    🎨 Format with black

--- position_traps_to_geojson/plot_position_traps.py
+++ position_traps_to_geojson/plot_position_traps.py
@@ -3,21 +3,25 @@ import utm
 import matplotlib.pyplot as plt
 
 
-class LittleMap():
+class LittleMap:
     def __init__(self):
         self.geopandas = None
         self.island = None
+
     def load_data(self, original_data):
-        self.geopandas = add_geometry(original_data)
+        self.geopandas = self._add_geometry(original_data)
+
     def plot(self, output_path):
         self.geopandas.plot()
         if isinstance(self.island, geopandas.GeoDataFrame):
-            ax = self.island.plot(color="white", edgecolor = "black")
-            self.geopandas.plot(ax = ax)
+            ax = self.island.plot(color="white", edgecolor="black")
+            self.geopandas.plot(ax=ax)
         plt.savefig(output_path)
+
     def read_island(self, shp_path):
         self.island = geopandas.read_file(shp_path)
 
-
-def add_geometry(df):
-    return geopandas.GeoDataFrame(df, geometry=geopandas.points_from_xy(df["Coor-X"], df["Coor-Y"]))
+    def _add_geometry(self, df):
+        return geopandas.GeoDataFrame(
+            df, geometry=geopandas.points_from_xy(df["Coor-X"], df["Coor-Y"])
+        )
