b66cae4577eec393c58f1e3c8219973c067010c1
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-07 09:12:04 -0800

    🏷️ Update version

--- position_traps_to_geojson/__init__.py
+++ position_traps_to_geojson/__init__.py
@@ -1,5 +1,5 @@
-"""A template Python module"""
+"""Module to plot map with active traps"""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 from .transformations import *  # noqa
 from .plot_position_traps import *  # noqa
